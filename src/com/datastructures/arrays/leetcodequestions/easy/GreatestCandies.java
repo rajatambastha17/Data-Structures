2740ee525ef54595668f0aa42aa91098112c8df5
Rajat Ambastha <rajatambastha0034@gmail.com>
2022-04-10 23:27:37 +0530

    Added some questions

---
+++ src/com/datastructures/arrays/leetcodequestions/easy/GreatestCandies.java
@@ -0,0 +1,40 @@
+package com.datastructures.arrays.leetcodequestions.easy;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 1431. Kids With the Greatest Number of Candies
+ */
+public class GreatestCandies {
+    public static void main(String[] args) {
+        int[] arr = {5,4,1,2,6};
+        int extraCandies = 4;
+        List<Boolean> ls = kidsWithCandies(arr, extraCandies);
+        System.out.println(ls);
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = getMax(candies);
+        Boolean[] arr = new Boolean[candies.length];
+        for (int i = 0; i < candies.length; i++) {
+            if ((candies[i] + extraCandies) >= max) {
+                arr[i] = true;
+            }
+            else {
+                arr[i] = false;
+            }
+        }
+        return Arrays.asList(arr);
+    }
+
+    public static int getMax(int[] arr) {
+        int max = 0;
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
---
+++ src/com/datastructures/arrays/leetcodequestions/easy/ShuffleArray.java
@@ -0,0 +1,29 @@
+package com.datastructures.arrays.leetcodequestions.easy;
+
+/**
+ * 1470. Shuffle the Array
+ */
+public class ShuffleArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 6, 5, 2, 3};
+        System.out.println("Before shuffle: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n\nAfter sum: ");
+        arr = shuffle(arr, 4);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < n; i++) {
+            result[i * 2] = nums[i];
+            result[i * 2 + 1] = nums[i + n];
+        }
+        return result;
+    }
+}
