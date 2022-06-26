36d41b501ca03989c4a10655d65509d9bda2b723
Rajat Ambastha <rajatambastha0034@gmail.com>
2022-06-26 13:05:35 +0530

    code formatted

--- src/com/datastructures/arrays/search/BinarySearch.java
+++ src/com/datastructures/arrays/search/BinarySearch.java
@@ -6,27 +6,23 @@ public class BinarySearch {
         int[] arr = {2, 4, 6, 8, 9, 10, 13, 15, 17, 18, 19, 23, 36, 37, 41, 45, 47, 56, 89, 99, 121};
         System.out.println(binarySearch(arr, 123));
     }
+
     static String binarySearch(int[] arr, int target) {
         int start_Index = 0;
         int end_index = arr.length - 1;
 
-        while(start_Index <= end_index) {
-
-//            Line no. 16
+        while (start_Index <= end_index) {
 //            This is a better way to find middle element of the array
 //            This saves the integer values from overflowing if the value of start or end goes past MAX_VALUE of Integer
-            int mid = start_Index + (end_index - start_Index)/2;
-
-            if(target > arr[mid]) {
+            int mid = start_Index + (end_index - start_Index) / 2;
+            if (target > arr[mid]) {
                 start_Index = mid + 1;
-            }
-            else if(target < arr[mid]) {
+            } else if (target < arr[mid]) {
                 end_index = mid - 1;
-            }
-            else {
-                return "Yay! Found element "+target+" at index: "+mid;
+            } else {
+                return "Yay! Found element " + target + " at index: " + mid;
             }
         }
-        return "Sorry! "+target+" is not present in this array..";
+        return "Sorry! " + target + " is not present in this array..";
     }
 }
