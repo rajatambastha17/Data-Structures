639538d08ee64739668084bbce743f12e3cd4340
Rajat Ambastha <rajatambastha0034@gmail.com>
2022-06-26 13:09:40 +0530

    code-format

--- src/com/datastructures/arrays/search/LinearSearch.java
+++ src/com/datastructures/arrays/search/LinearSearch.java
@@ -1,7 +1,5 @@
 package com.datastructures.arrays.search;
 
-import java.util.Scanner;
-
 public class LinearSearch {
     public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
@@ -18,29 +16,24 @@ public class LinearSearch {
 //
 //        System.out.println(linearSearch(arr, findItem));
 
-        int[][] arr2 = {
-                {12, 7, 28, 56},
-                {8, 67, 84, 81},
-                {66, 25},
-                {19}
-        };
+        int[][] arr2 = {{12, 7, 28, 56}, {8, 67, 84, 81}, {66, 25}, {19}};
         System.out.println(search2DArray(arr2, 19));
     }
 
     static String linearSearch(int[] arr, int item) {
         for (int i = 0; i < arr.length; i++) {
-            if(item == arr[i]) {
-                return "Yay! "+item+" is present at index: "+i;
+            if (item == arr[i]) {
+                return "Yay! " + item + " is present at index: " + i;
             }
         }
-        return "Sorry, "+item+" does not exist in this array";
+        return "Sorry, " + item + " does not exist in this array";
     }
 
     static String search2DArray(int[][] arr, int item) {
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[row].length; col++) {
                 if (arr[row][col] == item) {
-                    return "Yay! "+item+" is present at row "+row+" column "+col;
+                    return "Yay! " + item + " is present at row " + row + " column " + col;
                 }
             }
         }
