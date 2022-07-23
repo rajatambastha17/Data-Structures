8634bd9dbd930a0a8597e4b0c80868932259e54f
Rajat Ambastha <rajatambastha0034@gmail.com>
2022-07-23 16:45:12 +0530

    Question added

--- src/com/datastructures/arrays/leetcodequestions/easy/patternbased/binarysearch/CelingOfLetter.java
+++ src/com/datastructures/arrays/leetcodequestions/easy/patternbased/binarysearch/CelingOfLetter.java
@@ -13,17 +13,17 @@ public class CelingOfLetter {
         System.out.println(result);
     }
 
-    public static char ceilingOfLetter(char[] arr, char letter) {
+    public static char ceilingOfLetter(char[] letters, char target) {
         int start = 0;
-        int end = arr.length - 1;
+        int end = letters.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (letter < arr[mid]) {
+            if (target < letters[mid]) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
-        return arr[start % arr.length];
+        return letters[start % letters.length];
     }
 }
