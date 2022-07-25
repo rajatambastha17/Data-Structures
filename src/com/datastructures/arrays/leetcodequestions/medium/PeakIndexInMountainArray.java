7d0c21eec411b0795bbb3cca78df68e21e83967e
Rajat Ambastha <rajatambastha0034@gmail.com>
2022-07-25 23:55:30 +0530

    Question added

---
+++ src/com/datastructures/arrays/leetcodequestions/medium/PeakIndexInMountainArray.java
@@ -0,0 +1,28 @@
+package com.datastructures.arrays.leetcodequestions.medium;
+
+//https://leetcode.com/problems/find-peak-element/
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+public class PeakIndexInMountainArray {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 5, 4, 3, 2};
+        System.out.println(peakIndexAndElement(arr));
+    }
+
+    public static String peakIndexAndElement(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return "Index number "+start+" contains the peak element : "+arr[start];
+    }
+}
